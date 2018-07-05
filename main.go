813f5f057533c0b50dbfb1175fde34783de68b94
David Mazieres <dm@uun.org>
2018-07-05 09:06:36 -0700

    dumb main

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "os"
+
+func main() {
+	var t Transaction
+	t.TimeBounds = &TimeBounds{MinTime: 12345}
+	t.Memo.Type = MEMO_TEXT
+	t.Memo.Text().Set("Enjoy this transaction")
+	t.Operations = append(t.Operations, Operation{})
+	t.Operations[0].Body.Type = CREATE_ACCOUNT
+	XdrPrint(os.Stdout, "", &t)
+}
