b0261a1bf484281faf3dc4aa991f82960e00ce36
David Mazieres <dm@uun.org>
2019-06-10 18:24:01 -0700

    FileChanged (not used yet)

--- stcdetail/lockfile.go
+++ stcdetail/lockfile.go
@@ -1,12 +1,69 @@
 package stcdetail
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
+	"reflect"
+	"strings"
 )
 
 type ErrIsDirectory string
 func (e ErrIsDirectory) Error() string {
 	return string(e) + ": is a directory"
+}
+
+type ErrFileHasChanged string
+func (e ErrFileHasChanged) Error() string {
+	return string(e) + ": file has changed since read"
+}
+
+func clearAtime(sys interface{}) bool {
+	v := reflect.ValueOf(sys)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	if v.Kind() != reflect.Struct {
+		return false
+	}
+	atv := v.FieldByNameFunc(func(s string)bool {
+		return strings.HasPrefix(s, "Atim")
+	})
+	if (atv != reflect.Value{}) {
+		atv.Set(reflect.Zero(atv.Type()))
+		return true
+	}
+	return false
+}
+
+// Return true if a file may have been changed between the times that
+// the two FileInfo structures were read.
+func FileChanged(a, b os.FileInfo) bool {
+	if a.ModTime() != b.ModTime() || a.Size() != b.Size() {
+		return true
+	}
+	sa, sb := a.Sys(), b.Sys()
+	if !clearAtime(sa) || !clearAtime(sb) {
+		fmt.Fprintf(os.Stderr, "Can't parse FileInfo.Sys()\n")
+	}
+	return reflect.DeepEqual(a, b)
+}
+
+func ReadFile(path string) ([]byte, os.FileInfo, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer f.Close()
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, nil, err
+	}
+	ret, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, nil, err
+	}
+	return ret, fi, nil
 }
 
 // Updates a file in a safe way, by first writing data to an
