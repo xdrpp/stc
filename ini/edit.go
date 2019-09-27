0d3520c525cf4c23a184faf18f85a86659dc17d3
David Mazieres <dm@uun.org>
2019-09-27 16:56:47 -0700

    include another example for GenericIniSink

---
+++ ini/edit.go
@@ -0,0 +1,226 @@
+package ini
+
+import (
+	"container/list"
+	"fmt"
+	"io"
+	"strings"
+)
+
+// You can parse an INI file into an IniEditor, Set, Del, or Add
+// key-value pairs, then write out the result using WriteTo.
+// Preserves most comments and file ordering.
+type IniEditor struct {
+	fragments list.List
+	secEnd    map[string]*list.Element
+	values    map[string][]*list.Element
+	lastSec   *IniSection
+}
+
+// Write the contents of IniEditor to a Writer after applying edits
+// have been made.
+func (ie *IniEditor) WriteTo(w io.Writer) (int64, error) {
+	var ret int64
+	for e := ie.fragments.Front(); e != nil; e = e.Next() {
+		n, err := w.Write(e.Value.([]byte))
+		ret += int64(n)
+		if err != nil {
+			return ret, err
+		}
+	}
+	return ret, nil
+}
+
+func (ie *IniEditor) String() string {
+	ret := strings.Builder{}
+	ie.WriteTo(&ret)
+	return ret.String()
+}
+
+// Delete all instances of a key from the file.
+func (ie *IniEditor) Del(is *IniSection, key string) {
+	k := IniQKey(is, key)
+	for _, e := range ie.values[k] {
+		ie.fragments.Remove(e)
+	}
+	delete(ie.values, k)
+}
+
+func iniLine(key, value string) []byte {
+	return []byte(fmt.Sprintf("\t%s = %s\n", key, EscapeIniValue(value)))
+}
+
+func (ie *IniEditor) newItem(is *IniSection, key, value string) *list.Element {
+	ss := is.String()
+	e, ok := ie.secEnd[ss]
+	if !ok {
+		e = ie.fragments.Back()
+		if ssb := []byte(ss+"\n"); e != nil && len(e.Value.([]byte)) == 0 {
+			e.Value = ssb
+		} else {
+			e = ie.fragments.PushBack(ssb)
+		}
+		e = ie.fragments.InsertAfter([]byte{}, e)
+		ie.secEnd[ss] = e
+	}
+	e = ie.fragments.InsertBefore(iniLine(key, value), e)
+	k := IniQKey(is, key)
+	ie.values[k] = append(ie.values[k], e)
+	return e
+}
+
+// Replace all instances of key with a single one equal to value.
+func (ie *IniEditor) Set(is *IniSection, key, value string) {
+	k := IniQKey(is, key)
+	vs := ie.values[k]
+	if len(vs) > 0 {
+		ie.values[k] = []*list.Element{
+			ie.fragments.InsertAfter(iniLine(key, value), vs[len(vs)-1]),
+		}
+		for _, e := range vs {
+			ie.fragments.Remove(e)
+		}
+	} else {
+		ie.newItem(is, key, value)
+	}
+}
+
+// Add a new instance of key to the file without deleting any previous
+// instance of the key.
+func (ie *IniEditor) Add(is *IniSection, key, value string) {
+	k := IniQKey(is, key)
+	vs := ie.values[k]
+	if len(vs) > 0 {
+		e := ie.fragments.InsertAfter(iniLine(key, value), vs[len(vs)-1])
+		ie.values[k] = append(vs, e)
+	} else {
+		ie.newItem(is, key, value)
+	}
+}
+
+func (ie *IniEditor) appendItem(r *IniRange) (e1, e2 *list.Element) {
+	if r.StartIndex > r.PrevEndIndex {
+		e1 = ie.fragments.PushBack(r.Input[r.PrevEndIndex:r.StartIndex])
+	}
+	if r.EndIndex > r.StartIndex {
+		e2 = ie.fragments.PushBack(r.Input[r.StartIndex:r.EndIndex])
+	}
+	if e1 == nil {
+		e1 = e2
+	}
+	return
+}
+
+// Called by IniParseContents; do not call directly.
+func (ie *IniEditor) Section(ss IniSecStart) error {
+	// git-config associates comments with following section
+	e, _ := ie.appendItem(&ss.IniRange)
+	ie.secEnd[ie.lastSec.String()] = e
+	ie.lastSec = &ss.IniSection
+	return nil
+}
+
+// Called by IniParseContents; do not call directly.
+func (ie *IniEditor) Item(ii IniItem) error {
+	k := ii.QKey()
+	_, e := ie.appendItem(&ii.IniRange)
+	ie.values[k] = append(ie.values[k], e)
+	return nil
+}
+
+// Called by IniParseContents; do not call directly.
+func (ie *IniEditor) Done(r IniRange) {
+	e, _ := ie.appendItem(&r)
+	if e == nil {
+		e = ie.fragments.PushBack([]byte{})
+	}
+	ie.secEnd[ie.lastSec.String()] = e
+	ie.lastSec = nil
+}
+
+// Create an IniEdit for a file with contents.  Note that filename is
+// only used for parse errors; the file must already be read before
+// calling this function.
+func NewIniEdit(filename string, contents []byte) (*IniEditor, error) {
+	ret := IniEditor{
+		secEnd: make(map[string]*list.Element),
+		values: make(map[string][]*list.Element),
+	}
+	err := IniParseContents(&ret, filename, contents)
+	return &ret, err
+}
+
+// A bunch of edits to be applied to an INI file.
+type IniEdits []func(*IniEditor)
+
+// Delete a key.  Invoke as Del(sec, subsec, key) or Del(sec, key).
+func (ie *IniEdits) Del(sec string, args...string) error {
+	s, k := &IniSection{Section:sec}, ""
+	switch len(args) {
+	case 1:
+		k = args[0]
+	case 2:
+		s.Subsection = &args[0]
+		k = args[1]
+	default:
+		return ErrInvalidNumArgs
+	}
+	if !s.Valid() {
+		return ErrInvalidSection
+	}
+	*ie = append(*ie, func(ie *IniEditor){ie.Del(s, k)})
+	return nil
+}
+
+// Add a key, value pair.  Invoke as Add(sec, subsec, key, value) or
+// Add(sec, key, value).
+func (ie *IniEdits) Add(sec string, args...string) error {
+	s, k, v := &IniSection{Section:sec}, "", ""
+	switch len(args) {
+	case 2:
+		k = args[0]
+		v = args[1]
+	case 3:
+		s.Subsection = &args[0]
+		k = args[1]
+		v = args[2]
+	default:
+		return ErrInvalidNumArgs
+	}
+	if !s.Valid() {
+		return ErrInvalidSection
+	}
+	*ie = append(*ie, func(ie *IniEditor){ie.Add(s, k, v)})
+	return nil
+}
+
+// Add a key, value pair.  Invoke as Set(sec, subsec, key, value) or
+// Set(sec, key, value).
+func (ie *IniEdits) Set(sec string, args...string) error {
+	s, k, v := &IniSection{Section:sec}, "", ""
+	switch len(args) {
+	case 2:
+		k = args[0]
+		v = args[1]
+	case 3:
+		s.Subsection = &args[0]
+		k = args[1]
+		v = args[2]
+	default:
+		return ErrInvalidNumArgs
+	}
+	if !s.Valid() {
+		return ErrInvalidSection
+	}
+	*ie = append(*ie, func(ie *IniEditor){ie.Set(s, k, v)})
+	return nil
+}
+
+// Apply edits.
+func (ie *IniEdits) Apply(target *IniEditor) {
+	for _, f := range *ie {
+		f(target)
+	}
+	*ie = nil
+}
+
---
+++ ini/sinker.go
@@ -0,0 +1,134 @@
+package ini
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+// A generic IniSink that uses fmt.Sscan to parse non-string fields.
+type GenericIniSink struct {
+	// If non-nil, only match this specific section (otherwise
+	// ignore).
+	Sec *IniSection
+
+	// Pointers to the fields that should be parsed.
+	Fields map[string]interface{}
+}
+
+func (s *GenericIniSink) AddField(name string, ptr interface{}) {
+	if s.Fields == nil {
+		s.Fields = make(map [string]interface{})
+	}
+	s.Fields[name] = ptr
+}
+
+var errNotStructPtr = errors.New("argument must be pointer to struct")
+
+// Populate a GenericIniSink with fields of a struct, using the field
+// name or or the ini struct field tag (`ini:"field-name"`) if one
+// exists.  Tag `ini:"-"` says to ignore a field.  Note that i must be
+// a pointer to a structure or this function will panic.
+func (s *GenericIniSink) AddStruct(i interface{}) {
+	v := reflect.ValueOf(i)
+	if v.Kind() != reflect.Ptr {
+		panic(errNotStructPtr)
+	}
+	v = v.Elem()
+	if v.Kind() != reflect.Struct {
+		panic(errNotStructPtr)
+	}
+	t := v.Type()
+	for i, n := 0, t.NumField(); i < n; i++ {
+		f := t.Field(i)
+		name := f.Tag.Get("ini")
+		if name == "-" {
+			continue
+		} else if name == "" {
+			name = strings.ReplaceAll(f.Name, "_", "-")
+		}
+		s.AddField(name, v.Field(i).Addr().Interface())
+	}
+}
+
+func (s *GenericIniSink) String() string {
+	out := strings.Builder{}
+	if s.Sec != nil {
+		fmt.Fprintf(&out, "%s\n", s.Sec.String())
+	}
+	for name, i := range s.Fields {
+		v := reflect.ValueOf(i).Elem().Interface()
+		fmt.Fprintf(&out, "\t%s = %s\n", name, EscapeIniValue(fmt.Sprint(v)))
+	}
+	return out.String()
+}
+
+func (s *GenericIniSink) Item(ii IniItem) error {
+	if s.Sec.Eq(ii.IniSection) {
+		if i, ok := s.Fields[ii.Key]; ok {
+			v := reflect.ValueOf(i).Elem()
+			if ii.Value == nil {
+				v.Set(reflect.Zero(v.Type()))
+			} else if v.Kind() == reflect.String {
+				v.SetString(ii.Val())
+			} else {
+				_, err := fmt.Sscan(*ii.Value, i)
+				return err
+			}
+			return nil
+		}
+	}
+	return nil
+}
+
+func (s *GenericIniSink) IniSink() IniSink {
+	return s
+}
+
+var _ IniSinker = &GenericIniSink{} // XXX
+
+type IniSinker interface {
+	IniSink() IniSink
+}
+
+type IniSinks []IniSink
+
+func (s IniSinks) Init() {
+	for i := range s {
+		if init, ok := s[i].(interface{ Init() }); ok {
+			init.Init()
+		}
+	}
+}
+func (s IniSinks) Item(ii IniItem) error {
+	for i := range s {
+		if err := s[i].Item(ii); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+func (s IniSinks) Section(ss IniSecStart) error {
+	for i := range s {
+		if sec, ok := s[i].(interface{ Section(IniSecStart) error }); ok {
+			if err := sec.Section(ss); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+func (s IniSinks) Done() {
+	for i := range s {
+		if done, ok := s[i].(interface{ Done() }); ok {
+			done.Done()
+		}
+	}
+}
+
+func (s IniSinks) IniSink() IniSink {
+	return s
+}
+
+var _ IniSinker = IniSinks{}
