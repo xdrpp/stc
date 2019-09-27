229b3aec41e92bd31a2264608d62c96d3564d5df
David Mazieres <dm@uun.org>
2019-09-27 16:30:01 -0700

    remove duplicate test

--- stcdetail/detail_test.go
+++ stcdetail/detail_test.go
@@ -3,7 +3,6 @@ package stcdetail_test
 import (
 	"fmt"
 	"github.com/xdrpp/stc"
-	"github.com/xdrpp/stc/ini"
 	"github.com/xdrpp/stc/stx"
 	"io/ioutil"
 	"math/rand"
@@ -285,43 +284,4 @@ func ExampleLockFile() error {
 	fmt.Fprintf(lf, "New file contents\n")
 
 	return lf.Commit()
-}
-
-func ExampleIniEdit() {
-	bini := []byte(
-`; Here's a comment
-[sec1]
-	key1 = val1
-	key2 = val2
-; Here's another comment
-[sec2]
-	key3 = val3
-`)
-	ie, _ := ini.NewIniEdit("", bini)
-	sec1 := ini.IniSection{Section: "sec1"}
-	sec2 := ini.IniSection{Section: "sec2"}
-	sec3 := ini.IniSection{Section: "sec3"}
-	ie.Add(&sec1, "key4", "val4")
-	ie.Del(&sec1, "key2")
-	ie.Set(&sec1, "key1", "second version of val1")
-	ie.Add(&sec1, "key2", "second version of key2")
-	ie.Set(&sec1, "key5", "val5")
-	ie.Set(&sec2, "key6", "val6")
-	ie.Set(&sec2, "key3", "second version of key3")
-	ie.Set(&sec3, "key7", "val7")
-
-	fmt.Print(ie.String())
-	// Output:
-	// ; Here's a comment
-	// [sec1]
-	// 	key1 = second version of val1
-	// 	key4 = val4
-	// 	key2 = second version of key2
-	// 	key5 = val5
-	// ; Here's another comment
-	// [sec2]
-	// 	key3 = second version of key3
-	// 	key6 = val6
-	// [sec3]
-	//	key7 = val7
 }
